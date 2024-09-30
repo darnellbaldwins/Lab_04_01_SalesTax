fe29566c159177607f4d5302ef6c280eb39b11b2
Darnell Baldwin <darnellbaldwin@Darnells-MacBook-Air.local>
2024-09-30 19:54:18 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+        public static void main(String[] args) {
+            double salesTax = 0.05;
+            int purchasePrice = 10;
+            double finalPurchasePrice;
+            finalPurchasePrice = salesTax + purchasePrice;
+            System.out.println("The final price including sales tax is " + finalPurchasePrice);
+        }
+    }
